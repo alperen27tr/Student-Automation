26613df6f78002cc35123bc524b9cf86852a2b9d
alperen27tr <salperen2722@gmail.com>
2024-05-17 15:42:27 +0300

    Add project files.

---
+++ OgrenciOtomasyon/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace OgrenciOtomasyon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.surnameTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.departmentTextBox = new System.Windows.Forms.TextBox();
+            this.TCnoTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.PhoneNuTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label8 = new System.Windows.Forms.Label();
+            this.addButton = new System.Windows.Forms.Button();
+            this.delButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(102, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "İsim:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(85, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Soy İsim:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(366, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "TC no:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(363, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Tel no:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(85, 172);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Bölüm:";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(192, 54);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(148, 22);
+            this.nameTextBox.TabIndex = 5;
+            // 
+            // surnameTextBox
+            // 
+            this.surnameTextBox.Location = new System.Drawing.Point(192, 111);
+            this.surnameTextBox.Name = "surnameTextBox";
+            this.surnameTextBox.Size = new System.Drawing.Size(148, 22);
+            this.surnameTextBox.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(308, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(143, 26);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Öğrenci Kayıt";
+            // 
+            // departmentTextBox
+            // 
+            this.departmentTextBox.Location = new System.Drawing.Point(192, 172);
+            this.departmentTextBox.Name = "departmentTextBox";
+            this.departmentTextBox.Size = new System.Drawing.Size(148, 22);
+            this.departmentTextBox.TabIndex = 8;
+            // 
+            // TCnoTextBox
+            // 
+            this.TCnoTextBox.Location = new System.Drawing.Point(470, 54);
+            this.TCnoTextBox.Mask = "00000000000";
+            this.TCnoTextBox.Name = "TCnoTextBox";
+            this.TCnoTextBox.Size = new System.Drawing.Size(148, 22);
+            this.TCnoTextBox.TabIndex = 9;
+            this.TCnoTextBox.ValidatingType = typeof(int);
+            // 
+            // PhoneNuTextBox
+            // 
+            this.PhoneNuTextBox.Location = new System.Drawing.Point(470, 111);
+            this.PhoneNuTextBox.Mask = "(999) 000-0000";
+            this.PhoneNuTextBox.Name = "PhoneNuTextBox";
+            this.PhoneNuTextBox.Size = new System.Drawing.Size(148, 22);
+            this.PhoneNuTextBox.TabIndex = 10;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(32, 318);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(706, 204);
+            this.dataGridView1.TabIndex = 11;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellMouseEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellMouseEnter);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(308, 290);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(141, 25);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Öğrenci Listesi";
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(491, 172);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(91, 29);
+            this.addButton.TabIndex = 15;
+            this.addButton.Text = "Ekle";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // delButton
+            // 
+            this.delButton.Location = new System.Drawing.Point(300, 528);
+            this.delButton.Name = "delButton";
+            this.delButton.Size = new System.Drawing.Size(169, 36);
+            this.delButton.TabIndex = 17;
+            this.delButton.Text = "Sil";
+            this.delButton.UseVisualStyleBackColor = true;
+            this.delButton.Click += new System.EventHandler(this.delButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 607);
+            this.Controls.Add(this.delButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.PhoneNuTextBox);
+            this.Controls.Add(this.TCnoTextBox);
+            this.Controls.Add(this.departmentTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.surnameTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox surnameTextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox departmentTextBox;
+        private System.Windows.Forms.MaskedTextBox TCnoTextBox;
+        private System.Windows.Forms.MaskedTextBox PhoneNuTextBox;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button delButton;
+    }
+}
+
---
+++ OgrenciOtomasyon/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace OgrenciOtomasyon
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection baglanti;
+        SqlCommand komut;
+        SqlDataAdapter da;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void OgrenciGetir()
+        {
+            baglanti= new SqlConnection("server=.;Initial Catalog=OgrenciOtomasyon;Integrated Security=SSPI");
+            baglanti.Open();
+            da = new SqlDataAdapter("SELECT*FROM OGRENCİLER", baglanti);
+            DataTable tablo = new DataTable();
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            baglanti.Close();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OgrenciGetir();
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            string sorgu = "INSERT INTO OGRENCİLER(OgrenciIsim,OgrenciSoyisim,TCno,Telno,Bolum) VALUES (@OgrenciIsim,@OgrenciSoyisim,@TCno,@Telno,@Bolum)";
+            komut=new SqlCommand(sorgu,baglanti);
+
+            komut.Parameters.AddWithValue("@OgrenciIsim", nameTextBox.Text);
+            komut.Parameters.AddWithValue("@OgrenciSoyisim",surnameTextBox.Text);
+            komut.Parameters.AddWithValue("@TCno",TCnoTextBox.Text);
+            komut.Parameters.AddWithValue("@Telno",PhoneNuTextBox.Text);
+            komut.Parameters.AddWithValue("@Bolum",departmentTextBox.Text);
+
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close() ;
+            OgrenciGetir();
+        }
+
+        private void dataGridView1_CellMouseEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            nameTextBox.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            surnameTextBox.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            TCnoTextBox.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            PhoneNuTextBox.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            departmentTextBox.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            
+        }
+
+        private void delButton_Click(object sender, EventArgs e)
+        {
+            string sorgu = "DELETE FROM OGRENCİLER WHERE TCno=@TCno";
+            komut = new SqlCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("@TCno", Convert.ToInt64(TCnoTextBox.Text));
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            OgrenciGetir() ;
+        }
+    }
+}
